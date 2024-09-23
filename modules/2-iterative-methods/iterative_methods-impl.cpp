71d7d2c527494d2af8a2d4a20c44cc191bcb3e3d
antilopinae <mikbas31@gmail.com>
2024-09-23 19:06:54 +0200

    Added formatting of iterative-methods module

--- modules/2-iterative-methods/iterative_methods-impl.cpp
+++ modules/2-iterative-methods/iterative_methods-impl.cpp
@@ -1,24 +1,20 @@
 module;
 
-#include <iostream>
 #include <Eigen/Dense>
+#include <iostream>
 
 module iterative_methods;
 
-void Algo::helloWorld()
-{
-    std::cout << "hello world\n";
-}
+void Algo::helloWorld() { std::cout << "hello world\n"; }
 
-void test_iterative_methods()
-{
-    std::cout << "Test modules" << std::endl;
+void test_iterative_methods() {
+  std::cout << "Test modules" << std::endl;
 
-    using Eigen::MatrixXd;
-    MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;
+  using Eigen::MatrixXd;
+  MatrixXd m(2, 2);
+  m(0, 0) = 3;
+  m(1, 0) = 2.5;
+  m(0, 1) = -1;
+  m(1, 1) = m(1, 0) + m(0, 1);
+  std::cout << m << std::endl;
 }
